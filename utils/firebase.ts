4e0c492919b2d93b00614fc708dc23ad88b37b9d
keatyat90 <keatyat_010706@hotmail.com>
2025-01-31 23:55:38 +0800

    update

--- utils/firebase.ts
+++ utils/firebase.ts
@@ -1,20 +1,23 @@
 // firebase.ts
 import { initializeApp } from 'firebase/app'; // Import initializeApp from Firebase SDK
 import { getFirestore, doc, getDoc, getDocs, setDoc, collection, addDoc } from 'firebase/firestore'; // Firestore function
+import { getAnalytics } from "firebase/analytics";
+// import { API_KEY, AUTH_DOMAIN, PROJECT_ID, STORAGE_BUCKET, MESSAGING_SENDER_ID, APP_ID, MEASUREMENT_ID } from '@env';
 
 // Replace this with your Firebase project's configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyClwJ-lJjLjgPs8CaiLYWqix9Dj94VpxwI", 
-    authDomain: "feikenscan.firebaseapp.com", 
-    projectId: "feikenscan", 
-    storageBucket: "feikenscan.firebasestorage.app", 
-    messagingSenderId: "873801766950", 
-    appId: "1:873801766950:web:525450023f20befe2eeee7", 
-    measurementId: "G-076T4JK9DY" 
+    apiKey: process.env.API_KEY, 
+    authDomain: process.env.AUTH_DOMAIN, 
+    projectId: process.env.PROJECT_ID, 
+    storageBucket: process.env.STORAGE_BUCKET, 
+    messagingSenderId: process.env.MESSAGING_SENDER_ID, 
+    appId: process.env.APP_ID, 
+    measurementId: process.env.MEASUREMENT_ID
 };
 
 // Initialize Firebase app
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 // Initialize Firestore and Auth services
 const db = getFirestore(app);
